441fea748b712519b3f61c59eca4ea5cda6c4cad
Jerry <25115324+jwu56@users.noreply.github.com>
2021-08-03 15:38:09 +1000

    Created

--- api/twitter.php
+++ api/twitter.php
@@ -1,13 +1,12 @@
 <?php
-$url = 'https://api.twitter.com/1.1/users/show.json';
+$url = 'https://api.twitter.com/1.1/users/show.json'.join("\n",$_GET);
 
 // use key 'http' even if you send the request to https://...
 var_dump(getallheaders());
 $options = array(
 'http' => array(
 'header'  => join("\n",getallheaders()),
-'method'  => 'GET',
-'content' => join("\n",$_GET)
+'method'  => 'GET'
 )
 );
 $context  = stream_context_create($options);
