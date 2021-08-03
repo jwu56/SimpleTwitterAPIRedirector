a662224002eee6b9e6ee22504010aba1298e0230
Jerry <25115324+jwu56@users.noreply.github.com>
2021-08-03 16:09:15 +1000

    Created

--- api/twitter.php
+++ api/twitter.php
@@ -1,15 +1,22 @@
 <?php
 $query = $_SERVER["QUERY_STRING"];
-$url = 'https://api.twitter.com/1.1/users/show.json'."?".$query;
-// use key 'http' even if you send the request to https://...
-$options = array(
-'http' => array(
-'header'  => "Authorization: ".getallheaders()["Authorization"]."\n",
-'method'  => 'GET'
-)
-);
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-if ($result === FALSE) { /* Handle error */ }
-echo($result);
+// make request
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Authorization: '.$_GET["Authorization"]
+));
+curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/1.1/users/show.json'."?".$query);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+
+// convert response
+$output = json_decode($output);
+
+// handle error; error output
+if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
+
+    var_dump($output);
+}
+
+curl_close($ch);
 ?>
