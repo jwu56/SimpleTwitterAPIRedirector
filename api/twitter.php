03e35531868e074ce231d40c7e39c7f8fb61badf
Jerry <25115324+jwu56@users.noreply.github.com>
2021-08-03 17:25:00 +1000

    Fix stupid mistakes and other stuff

--- api/twitter.php
+++ api/twitter.php
@@ -1,11 +1,10 @@
 <?php
-var_dump($_ENV["apikey"]);
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 // make request
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Authorization: Bearer-Token '.$_ENV["apikey"]
+    'Authorization: Bearer '.$_ENV["apikey"]
 ));
 curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/1.1/users/show.json?cursor=-1&screen_name=DiscordAnaxes&skip_status=true&include_user_entities=false');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
