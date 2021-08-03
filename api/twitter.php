2316045221964a296a21c7f0df7a6b02244e40e8
Jerry <25115324+jwu56@users.noreply.github.com>
2021-08-03 16:13:16 +1000

    Created

--- api/twitter.php
+++ api/twitter.php
@@ -8,9 +8,7 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 curl_setopt($ch, CURLOPT_URL, 'https://api.twitter.com/1.1/users/show.json'."?".$query);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $output = curl_exec($ch);
-
-// convert response
-$output = json_decode($output);
+echo($output);
 
 // handle error; error output
 if(curl_getinfo($ch, CURLINFO_HTTP_CODE) !== 200) {
